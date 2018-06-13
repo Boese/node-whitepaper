41042b75838d7ce1a660d4eba679d546953e52d3
Boese <chris.boese@intel.com>
2018-06-13 08:08:32 -0700

    Initial commit

---
+++ NET/http-server.js
@@ -0,0 +1,8 @@
+// Make FtP Server
+// - No dependencies
+// - Use Filewatcher (fs)
+// - Use streams
+// - Use cluster
+// - Use Async
+
+const { http } = require("http");
---
+++ NET/socket-server.js
@@ -0,0 +1,6 @@
+// Monitoring server (like kratos)
+    // no dependencies
+    // provide rooms
+    // provide client (c++ napi? OR child process/daemon client)
+    // provide database
+    // provide OS info to server
